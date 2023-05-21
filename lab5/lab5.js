25564119b7ea36bb94ded40d5a40ecbd8c35f579
SlaterJoseph <Josephslater03@gmail.com>
2023-05-20 23:52:22 -0400

    5th lab

--- lab5/lab5.js
+++ lab5/lab5.js
@@ -1,8 +1,23 @@
 window.addEventListener('DOMContentLoaded', (event) => {
-    const video = document.getElementById('myVideo');
-    video.addEventListener('ended', function() {
-      this.currentTime = 0;
-      this.play();
-    });
-  });
-  
+  const mediaContainer = document.getElementById('mediaContainer');
+  const video = document.createElement('video');
+  const image = document.createElement('img');
+
+  video.controls = true;
+  video.autoplay = true;
+  video.loop = true;
+  video.muted = true;
+
+  const videoSource = document.createElement('source');
+  videoSource.src = 'sea.mp4';
+  videoSource.type = 'video/mp4';
+  video.appendChild(videoSource);
+
+  video.onerror = function() {
+    // Fallback to GIF
+    image.src = 'sea.gif';
+    mediaContainer.appendChild(image);
+  };
+
+  mediaContainer.appendChild(video);
+});
